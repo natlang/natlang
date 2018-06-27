47f0227120471a58037a6405ecf642b954d51066
Natalie Lang <natalielang@MacBook-Pro-Natalie.local>
2018-06-27 15:31:11 +0100

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hello ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
